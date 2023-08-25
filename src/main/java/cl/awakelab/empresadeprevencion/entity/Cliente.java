b851fd72cb5f6615f2032c162cb18bd2886fb794
cpmeneses <cpmeneses94@gmail.com>
2023-08-24 20:57:32 -0400

    cliente atributos sin relaciones

--- src/main/java/cl/awakelab/empresadeprevencion/entity/Cliente.java
+++ src/main/java/cl/awakelab/empresadeprevencion/entity/Cliente.java
@@ -1,4 +1,46 @@
 package cl.awakelab.empresadeprevencion.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 12)
+    private String rut;
+
+    @Column(nullable = false, length = 200)
+    private String razonSocial;
+
+    @Column(nullable = false, length = 100)
+    private String nombreContacto;
+
+    @Column(nullable = false, length = 100)
+    private String direccion;
+
+    @Column(nullable = false, length = 15)
+    private String telefono;
+
+    @Column(nullable = false, length = 50)
+    private String correoElectronico;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaIngreso;
+
+    @Column(nullable = false)
+    private boolean estado;
+
+
 }
